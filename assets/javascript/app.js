4b8377918807cbbf33435c741be54042f351c3e3
GoatFeatherFarm <44263291+missybarringer@users.noreply.github.com>
2019-03-04 12:03:37 -0600

    updates & comments added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,73 +12,87 @@
 // --*Bonus* include a 1-click download button for each gif that works on all devices
 // --*Bonus* integrate this search with additional APIs.
 // --*Bonus* allow users to add their fav gifs to a 'favorites' section
-$(document).ready(function () {
-  // set up variable topics with values
-  var gifs=["pizza","sushi","mexican food","burgers"];
 
+$(document).ready(function () {
+  // set up variable topics with values *** got the CORES error with that variable so used gifs instead
+  var gifs=["Loop","3d","Pixel","Art","Psychedelic","Collage"];
+
+// function to display the Gifs
 function displaygifShow () {
   var gif = $(this).attr("data-name");
+  // Storing the giphy API URL for gifs using the art name
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-  // topics + "_s.gif&api_key=Td2YRT2jyEnZurMPZcbBsjKTTj7LSEug&limit=10";
   gif + "&api_key=Td2YRT2jyEnZurMPZcbBsjKTTj7LSEug&limit=10";
 
-  // AJAX call for the gif buttons
+  // performing an AJAX GET request to our queryURL
   $.ajax({
       url: queryURL,
       method: "GET"
+  // After the data from the AJAX request comes back
   }).then(function(response){
-      $("#gifview").empty();
+      $("#gifView").empty();
+      // storing the data from the AJAX request in the results variable
       var results = response.data;
-
+      // looping through the returned results and creating variables for the divs & rating
       for (var i = 0; i < results.length; i++) {
           var gifDiv = $("<div>");
           gifDiv.addClass("gifpictures");
           var rating = results[i].rating;
-          var p = $("<h2>").text("Rating: " + rating);
-
+          var p = $("<h4>").text("Rating: " + rating);
+          // creating & storing an image tag in gifImage variable
           var gifImage = $("<img>");
-          gifImage.attr("src", results[i].images.fixed_height_still.url);
-          gifImage.attr("data-still", results[i].images.fixed_height_still.url);
-          gifImage.attr("data-animate", results[i].images.fixed_height.url);
+          // assigning attributes & class for still & animated image URLs
+          gifImage.attr("src", results[i].images.fixed_height_small_still.url);
+          gifImage.attr("data-still", results[i].images.fixed_height_small_still.url);
+          gifImage.attr("data-animate", results[i].images.fixed_height_small.url);
           gifImage.attr("data-state", "still");
           gifImage.addClass("gifImage");
-
+          // prepending the rating for display
           gifDiv.prepend(p);
-
+          // prepending the gifImage and gifDiv for display
           gifDiv.prepend(gifImage);
-          $("#gifview").prepend(gifDiv);
+          $("#gifView").prepend(gifDiv);
       }
-
+      // creating the click event, if the images's state is still, update its src attr to the data-animate value.
       $(".gifImage").on("click", function() {
           var state = $(this).attr("data-state");
           if (state === "still") {
               $(this).attr("src", $(this).attr("data-animate"));
+              // then set the image's data-state to animate
               $(this).attr("data-state", "animate");
           } else {
+              // else set the src to the data-still value
               $(this).attr("src", $(this).attr("data-still"));
               $(this).attr("data-state", "still");
           }
         });
     });
   }
+  // make sure the #gifButtons div is empty then loop through the gifs(topics) and add a button tag to each one with class, attr, & text
+  // then append to the #gifButtons div for display
   function renderButtons() {
-      $("#gifbuttons").empty();
+      $("#gifButtons").empty();
       for (var i = 0; i < gifs.length; i++) {
           var gifAdd = $("<button>");
           gifAdd.addClass("gif");
           gifAdd.attr("data-name", gifs[i]);
           gifAdd.text(gifs[i]);
-          $("#gifbuttons").append(gifAdd);
+          $("#gifButtons").append(gifAdd);
       }
     }
-
+    // creating the click event to accept text inputted by the user and push it into the gifs(topics) array.
     $("#add-gif").on("click", function(event) {
         event.preventDefault();
+        // this grabs the text inputted and trims any leading or trailing spaces
         var gif = $("#gif-input").val().trim();
+        // the text is added to the initial array that was set up
         gifs.push(gif);
+
+        // calling the renderButtons function to process the array
         renderButtons();
     });
-
+    // adding click event listeners to all elements with a class of gif
     $(document).on("click", ".gif", displaygifShow);
+    // calling the renderButtons function to display initial buttons
     renderButtons();
 });
